2caf381c03fd9a92211fed6e38b7856da019a2c8
clesteria <129359043+clesteria@users.noreply.github.com>
2025-10-06 19:55:55 +0900

    PowerShell関係の記述を整頓、Linux用Homebrewの依存関係チェック関数を追加

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -1,30 +1,45 @@
 ﻿#!/usr/bin/env pwsh
 
 # 変数
+New-Variable -Name 'ProfileRoot' -Value $(Split-Path -Path $PROFILE.CurrentUserAllHosts) -Scope 'Script'
+New-Variable -Name 'InitialRoot' -Value $(Join-Path -Path $PSScriptRoot -ChildPath 'PowerShell') -Scope 'Script'
+New-Variable -Name 'PathDelimiter' -Value $(if ( $env:PSModulePath -match ';' ) { ';' } else { ':' }) -Scope 'Script'
 
-$ProfileRoot = Split-Path $PROFILE
-$InitialRoot = Join-Path $PSScriptRoot 'PowerShell'
-$PathDelimiter = if ( $env:PSModulePath -match ';' ) { ';' } else { ':' }
-
-# Profile用ディレクトリ、シンボリックリンク作成
+# Profile用ディレクトリ作成
 
 (
-  @{ ItemType = 'Directory'; Path = $ProfileRoot; Force = $true },
-  @{ ItemType = 'SymbolicLink'; Path = $env:PSModulePath.Split($PathDelimiter)[0]; Value = (Join-Path $InitialRoot 'Modules') },
-  @{ ItemType = 'SymbolicLink'; Path = $PROFILE.CurrentUserAllHosts; Value = (Join-Path $InitialRoot 'profile.ps1') },
-  @{ ItemType = 'SymbolicLink'; Path = $PROFILE; Value = (Join-Path $InitialRoot (Split-Path -Leaf $PROFILE)) },
-  @{ ItemType = 'SymbolicLink'; Path = '~\.fontlist'; Value = (Join-Path $PSScriptRoot '.fontlist') }
-) | % {
+  @{ ItemType = 'Directory'; Path = $ProfileRoot; Force = $true }
+).ForEach({
   ## 配布先が既に存在する場合、次のエントリへ進む
-  if ( Test-Path $_.Path ) {
-    return
-  }
-
-  ## 配布元が存在しない場合、次のエントリへ進む(ISE用)
-  if ( ! (Test-Path $_.Value) ) {
+  if ( Test-Path -Path $_.Path -ErrorAction 'SilentlyContinue') {
+    Write-Host ($_.Path + " is existed.")
     return
   }
 
   ## アイテムを作成
-  ni @_
-}
+  New-Item @_
+})
+
+# シンボリックリンク作成
+
+(
+  @{ ItemType = 'SymbolicLink'; Path = $env:PSModulePath.Split($PathDelimiter)[0]; Value = (Join-Path $InitialRoot 'Modules') },
+  @{ ItemType = 'SymbolicLink'; Path = $PROFILE.CurrentUserAllHosts; Value = (Join-Path $InitialRoot 'profile.ps1') },
+  @{ ItemType = 'SymbolicLink'; Path = $PROFILE; Value = (Join-Path $InitialRoot (Split-Path -Leaf $PROFILE)) },
+  @{ ItemType = 'SymbolicLink'; Path = '~\.fontlist'; Value = (Join-Path $PSScriptRoot '.fontlist') }
+).ForEach({
+  ## 配布先が既に存在する場合、次のエントリへ進む
+  if ( Test-Path $_.Path -ErrorAction 'SilentlyContinue') {
+    Write-Host ($_.Path + " is existed.")
+    return
+  }
+
+  ## 配布元が存在しない場合、次のエントリへ進む(ISE用)
+  if ( ! (Test-Path $_.Value -ErrorAction 'SilentlyContinue') ) {
+    Write-Host ($_.Value + " is not existed.")
+    return
+  }
+
+  ## アイテムを作成
+  New-Item @_
+})
--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -34,6 +34,7 @@ else {
 [ScriptBlock]$Global:Prompt = {
   if ( $Global:DisplayDate ) {
     Write-Host ('{0}[{1}]' -f "`n", (& $Global:DisplayDate)) -ForegroundColor Yellow -NoNewline
+    Write-Host ' <PowerShell>' -ForegroundColor Green -NoNewline
     Write-Host (' {0} ' -f $Pwd.ProviderPath.Replace($HOME, '~')) -ForegroundColor Cyan
   }
 
@@ -45,42 +46,13 @@ else {
   }
 }
 
-## UHD確認
-if ( $IsWindows ) {
-  $Horizontal, $Vertical = Get-CimInstance -ClassName Win32_VideoController | ForEach-Object { $_.CurrentHorizontalResolution, $_.CurrentVerticalResolution }
-}
-#elseif ( $IsMacOS ) {
-#  $Display = /usr/sbin/system_profiler SPDisplaysDataType | ? { $_.Split(':')[0].Trim() -eq 'UI Looks like' } | % { $_.Trim().Split(':')[1].Trim() }
-#  $Horizontal = $Display.Split()[0] | Sort-Object -Unique | select -Last 1
-#  $Vertical = $Display.Split()[2] | Sort-Object -Unique | select -Last 1
-#}
-elseif ( $IsLinux ) {
-  $Horizontal = 2560
-  $Vertical = 1440
-}
-
-[Boolean]$Global:IsUHD = $Horizontal -gt 1920 -and $Vertical -gt 1080
-
 # 変数設定
 
 [String[]]$Global:ProgramFiles = ($env:ProgramFiles, ${env:ProgramFiles(x86)})
 [String]$Global:ProfileRoot = Split-Path $PROFILE
 [String]$Global:WorkplaceProfile = Join-Path $ProfileRoot 'WorkplaceProfile.ps1'
-[String]$Global:DefaultFont = if ( $Global:IsUHD ) { 'Ricty Discord' } else { '恵梨沙フォント+Osaka－等幅' }
 [String]$Global:GitPath = '~/Git'
 [String]$Global:PathDelimiter = if ( $env:PSModulePath -match ';' ) { ';' } else { ':' }
-
-# エイリアス設定
-
-#(
-#  @{ Name = 'cd'; Value = 'Set-CurrentDirectory'; Option = 'AllScope'; Scope = 'Global' },
-#  @{ Name = 'which'; Value = 'WhereIs-Command'; Option = 'AllScope'; Scope = 'Global' },
-#  @{ Name = 'whereis'; Value = 'WhereIs-Command'; Option = 'AllScope'; Scope = 'Global' },
-#  @{ Name = 'time'; Value = 'Get-ScriptTime'; Option = 'AllScope'; Scope = 'Global' },
-#  @{ Name = 'find'; Value = 'Find-ChildItem'; Option = 'AllScope'; Scope = 'Global' }
-#) | ForEach-Object {
-#  Set-Alias @_
-#}
 
 ## クリップボード格納コマンド統一
 if ( $IsMacOS ) {
@@ -107,15 +79,15 @@ if ( $IsMacOS ) {
 
 & {
   $ErrorActionPreference = 'SilentlyContinue'
-  (
+  @(
     (Split-Path $PROFILE), # Profile
     ($ProgramFiles | Get-ChildItem -Directory | Where-Object -Property Name -match 'vim' | Get-ChildItem | Where-Object -Property Name -match '^vim').DirectoryName, # vim
     ('C:\Windows\Microsoft.NET\Framework64' | Get-ChildItem -Directory | Get-ChildItem | Where-Object -Property Name -eq 'csc.exe' | Sort-Object VersionInfo)[-1].DirectoryName # .NET Framework
-  ) | ForEach-Object {
+  ).ForEach.({
     if ( $env:PATH.Split($PathDelimiter) -notcontains $_ ) {
       $env:PATH += ($PathDelimiter + $_)
     }
-  }
+  })
 }
 
 # LOCAL MACHINEとCURRENT USER以外のレジストリをマウント
@@ -125,17 +97,17 @@ if ( $IsWindows ) {
     @{ Name = 'HKCR'; PSProvider = 'Registry'; Root = 'HKEY_CLASSES_ROOT' },
     @{ Name = 'HKU'; PSProvider = 'Registry'; Root = 'HKEY_USERS' },
     @{ Name = 'HKCC'; PSProvider = 'Registry'; Root = 'HKEY_CURRENT_CONFIG' }
-  ) | ForEach-Object {
-    ## Windows以外でレジストリの場合はスキップ
+  ).ForEach({
+    ## レジストリの場合はスキップ
     if ( $_.PSProvider -eq 'Registry' ) {
       return
     }
 
     ## なければ追加
     if ( ! (Test-Path ('{0}:' -f $_.Name)) ) {
-      New-PSDrive @_ > $null
+      [void](New-PSDrive @_)
     }
-  }
+  })
 }
 
 # プロンプト設定
@@ -148,6 +120,6 @@ Switch-Prompt
 
 # 環境別プロファイルを読み込み(場所により異なる設定が必要な場合に使用)
 
-if ( Test-Path $WorkplaceProfile -ErrorAction SilentlyContinue ) {
+if ( Test-Path -Path $WorkplaceProfile -ErrorAction 'SilentlyContinue' ) {
   . $WorkplaceProfile
 }
